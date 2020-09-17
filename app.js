8d144dbe092f47dd090f1413127463d57d7fccc4
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-09-17 15:27:22 -0700

    proj1

--- app.js
+++ app.js
@@ -4,24 +4,7 @@ if (typeof $ == 'undefined') {
 	console.log('I did it! I linked jQuery and this js file properly!');
 }
 
-let sheetUrl =
-	'https://docs.google.com/spreadsheets/d/16oWt5naht2TV0UfEzlgu71On8uSEKm9znmuOmXaNR24/edit?usp=sharing';
 
-let sheetAsJSON =
-	'https://spreadsheets.google.com/feeds/list/16oWt5naht2TV0UfEzlgu71On8uSEKm9znmuOmXaNR24/od6/public/values?alt=json';
-
-$.ajax({ url: sheetAsJSON }).then((data) => {
-	
-	const projects = data.feed.entry.map((project) => {
-		return {
-			title: project.gsx$title.$t,
-			image: project.gsx$image.$t,
-			description: project.gsx$description.$t,
-		};
-	});
-	console.log('this is data:', data.feed.entry[0].gsx$title.$t);
-	console.log('this is project:', projects);
-	});
 
 const myBtn = document.getElementById("myBtn")
 
@@ -43,37 +26,59 @@ function topFunction() {
 }
 
 
-const projectsArr = [
-	{
-		title: 'Responsive',
-		img:
-			'https://res.cloudinary.com/dv01780vo/image/upload/v1600211142/Screen_Shot_2020-09-15_at_4.04.59_PM_vr10ee.png',
-	},
-	{
-		title: 'Tamagotchi',
-		img:
-			'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
-	},
-];
+// const projectsArr = [
+// 	{
+// 		title: 'Responsive',
+// 		img:
+// 			'https://res.cloudinary.com/dv01780vo/image/upload/v1600211142/Screen_Shot_2020-09-15_at_4.04.59_PM_vr10ee.png',
+// 	},
+// 	{
+// 		title: 'Tamagotchi',
+// 		img:
+// 			'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
+// 	},
+// ];
 
-projectsArr.forEach((project) => {
-	$('.projects').append(`
-<div class="card-deck">
-  <div class="card">
-    <img src=${project.img} class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${project.title}</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src=${project.img} class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${project.title}</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-`);
+
+let sheetUrl =
+	'https://docs.google.com/spreadsheets/d/16oWt5naht2TV0UfEzlgu71On8uSEKm9znmuOmXaNR24/edit?usp=sharing';
+
+let sheetAsJSON =
+	'https://spreadsheets.google.com/feeds/list/16oWt5naht2TV0UfEzlgu71On8uSEKm9znmuOmXaNR24/od6/public/values?alt=json';
+
+$.ajax({ url: sheetAsJSON }).then((data) => {
+	// console.log('this is data', data)
+	const projects = data.feed.entry.map((project) => {
+		return {
+			title: project.gsx$title.$t,
+			image: project.gsx$image.$t,
+			description: project.gsx$description.$t,
+			link: project.gsx$link.$t,
+		};
+	});
+
+	renderCards(projects)
+	console.log('these are projects', projects)
+	// console.log('this is data:', data.feed.entry[0].gsx$title.$t);
+	console.log('this is project:', projects);
 });
+
+const renderCards = (projectsArr) => {
+	projectsArr.forEach( project => {
+  $('.projects').append(`
+<div class="row">
+  <div class="col-sm-6">
+	<div class="card">
+		<img src=${project.image} class="card-img-top" alt="..."></img>
+      		<div class="card-body">
+        		<h5 class="card-title">${project.title}</h5>
+        		<p class="card-text">${project.description}</p>
+        	<a href="#" class="btn btn-primary">${project.link}</a>
+    </div>
+  </div>
+</div>
+
+`)
+})
+
+}
