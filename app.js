62d977011e5029b346960834190348a32e22fd43
Jaime Bondoc <starktower@Jaimes-MacBook-Air.local>
2020-09-16 17:54:20 -0700

    proj1

--- app.js
+++ app.js
@@ -23,6 +23,23 @@ $.ajax({ url: sheetAsJSON }).then((data) => {
 	console.log('this is project:', projects);
 	});
 
+window.onscroll = function() {
+	scrollFunction()
+}
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myBtn.style.display = "block"
+	} else {
+		myBtn.style.display = "none"
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0
+	document.documentElement.scrollTop = 0
+}
+
 
 const projectsArr = [
 	{
